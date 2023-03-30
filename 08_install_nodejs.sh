4d3f417b3fbd7f035a8970fa5c99fb0e4ff90f1d
James Tayler <james@tayler.io>
2023-03-30 23:28:38 +1300

    update to newer versions

--- 08_install_nodejs.sh
+++ 08_install_nodejs.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 sudo apt-get install -y software-properties-common
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 
 sudo apt-get update
 sudo apt-get install -y nodejs
